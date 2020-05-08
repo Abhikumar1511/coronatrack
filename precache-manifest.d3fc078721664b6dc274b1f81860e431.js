c1e6f4812fa7568046bbf00dc2318ab1ded90000
Abhikumar1511 <65035617+Abhikumar1511@users.noreply.github.com>
2020-05-09 01:37:35 +0530

    Added new files

---
+++ precache-manifest.d3fc078721664b6dc274b1f81860e431.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f7f45baf8baf90e6df47e648ca251b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "923cd6c4707a509fe5d8",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f9123d0bcedf2c7daa51",
+    "url": "/static/css/main.0eca5115.chunk.css"
+  },
+  {
+    "revision": "923cd6c4707a509fe5d8",
+    "url": "/static/js/2.ba79e6a7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ba79e6a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9123d0bcedf2c7daa51",
+    "url": "/static/js/main.9392bd16.chunk.js"
+  },
+  {
+    "revision": "01332cadfa4dcced2a9f",
+    "url": "/static/js/runtime-main.0c3f9f15.js"
+  },
+  {
+    "revision": "7c15d7bb7deb262ae954248b78d40215",
+    "url": "/static/media/capsule.7c15d7bb.jpg"
+  },
+  {
+    "revision": "19047b8126c31e2b8a2ff57a0a46462c",
+    "url": "/static/media/doc.19047b81.jpg"
+  },
+  {
+    "revision": "7757a40a803f94f54cc937689e40c62b",
+    "url": "/static/media/fvr.7757a40a.jpg"
+  },
+  {
+    "revision": "74fcc656d72584ae75d9fe0b8be24d81",
+    "url": "/static/media/gocorona.74fcc656.jpg"
+  },
+  {
+    "revision": "c973e3565d85d3bfadf8f86bda3db521",
+    "url": "/static/media/handw.c973e356.jpg"
+  },
+  {
+    "revision": "489eb170e0ddb83fcfb0fe103135a191",
+    "url": "/static/media/heart.489eb170.jpg"
+  },
+  {
+    "revision": "1f01d0e14738f28132fcaebc504b73ca",
+    "url": "/static/media/ind.1f01d0e1.png"
+  },
+  {
+    "revision": "7ae5ae896ceeddad75ac96f65749620e",
+    "url": "/static/media/mask.7ae5ae89.jpg"
+  },
+  {
+    "revision": "247853a0a0f9c8eecd4687cc339240f5",
+    "url": "/static/media/news1.247853a0.jpg"
+  },
+  {
+    "revision": "cc53a74b2bc0ee58d36026a25717408f",
+    "url": "/static/media/sani.cc53a74b.jpg"
+  }
+]);
